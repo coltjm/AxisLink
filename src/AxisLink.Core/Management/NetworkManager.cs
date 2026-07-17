f94904e1c88b9bb3b6618d4a7d7df4974a08f48e
coltjm <coltjmcguire@gmail.com>
2026-07-17 07:21:53 -0700

    Created blank network manager

---
+++ src/AxisLink.Core/Management/NetworkManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AxisLink.Core.Management
+{
+    public class NetworkManager
+    {
+        public NetworkManager() 
+        { 
+            
+        }
+
+        public void ScanControllers()
+        {
+            // Logic to scan for controllers on the network
+        }
+    }
+}
--- src/AxisLink.Desktop/ConsoleTesting.cs
+++ src/AxisLink.Desktop/ConsoleTesting.cs
@@ -4,8 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace AxisLink.Desktop
 {
@@ -31,15 +33,101 @@ namespace AxisLink.Desktop
 
                     logger.LogInfo("[Background] All network connections finalized successfully.");
                     throw new Exception("Simulated connection failure for testing purposes."); // Simulate a connection failure
+
                 }
                 catch (Exception ex)
                 {
                     logger.LogError($"[Background ERROR] Connection failed: {ex.Message}");
                 }
+                ShowNetworkInterfaces(logger);
             });
 
             // 3. UI/Framework initialization finishes instantly here
             logger.LogInfo("=== MAIN THREAD INITIALIZATION COMPLETE ===");
+        }
+        public static void ShowNetworkInterfaces(IConsoleLogger logger)
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            logger.LogInfo("Interface information for " + computerProperties.HostName + "." + computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)
+            {
+                logger.LogInfo("  No network interfaces found.");
+                return;
+            }
+
+            logger.LogInfo("  Number of interfaces .................... : " + nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                if(adapter.GetPhysicalAddress().ToString() != "" && adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet && adapter.GetIPProperties().UnicastAddresses.Count > 0)
+                { 
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    logger.LogInfo(adapter.Description);
+                    logger.LogInfo(String.Empty.PadLeft(adapter.Description.Length, '='));
+                    logger.LogInfo("  Interface type .......................... : " + adapter.NetworkInterfaceType);
+                    logger.LogInfo("  Physical Address ........................ : " + adapter.GetPhysicalAddress().ToString());
+                    logger.LogInfo("  Operational status ...................... : " + adapter.OperationalStatus);
+                    string versions = "";
+
+                    // Create a display string for the supported IP versions.
+                    if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                    {
+                        versions = "IPv4";
+                    }
+                    if (adapter.Supports(NetworkInterfaceComponent.IPv6))
+                    {
+                        if (versions.Length > 0)
+                        {
+                            versions += " ";
+                        }
+                        versions += "IPv6";
+                    }
+                    logger.LogInfo("  IP version .............................. : " + versions);
+                    //logger.LogInfo("  IP Address ............................... : "+ properties.UnicastAddresses.ToString());
+                    logger.LogInfo("  Name.................................. : " + adapter.Name);
+                    //ShowIPAddresses(properties);
+                    UnicastIPAddressInformationCollection unicast = properties.UnicastAddresses;
+                    if (unicast.Count > 0)
+                    {
+                        logger.LogInfo("  Unicast Addresses ....................... :");
+                        foreach (UnicastIPAddressInformation ip in unicast)
+                        {
+                            logger.LogInfo("    " + ip.Address.ToString());
+                            logger.LogInfo("    Subnet Mask ............................ : " + ip.IPv4Mask.ToString());
+                            
+                        }
+                    }
+                    // The following information is not useful for loopback adapters.
+                    if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                    {
+                        continue;
+                    }
+                    logger.LogInfo("  DNS suffix .............................. : " + properties.DnsSuffix);
+
+                    string label;
+                    if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                    {
+                        IPv4InterfaceProperties ipv4 = properties.GetIPv4Properties();
+                        logger.LogInfo("  MTU...................................... : " + ipv4.Mtu);
+                        if (ipv4.UsesWins)
+                        {
+
+                            IPAddressCollection winsServers = properties.WinsServersAddresses;
+                            if (winsServers.Count > 0)
+                            {
+                                label = "  WINS Servers ............................ :";
+                                //ShowIPAddresses(label, winsServers);
+                            }
+                        }
+                    }
+
+                    logger.LogInfo("  DNS enabled ............................. : " + properties.IsDnsEnabled);
+                    logger.LogInfo("  Dynamically configured DNS .............. : " + properties.IsDynamicDnsEnabled);
+                    logger.LogInfo("  Receive Only ............................ : " + adapter.IsReceiveOnly);
+                    logger.LogInfo("  Multicast ............................... : " + adapter.SupportsMulticast);
+                    //ShowInterfaceStatistics(adapter);
+                }
+            }
         }
     }
 }
